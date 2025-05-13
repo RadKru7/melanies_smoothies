23094db8d4e9d219061a78aab3fb3f627ada596f
RadKru7 <kruszewski.radek@gmail.com>
2025-05-13 10:33:01 +0200

    Update streamlit_app.py
    zmiany sesji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -13,13 +12,11 @@ st.write(
   """
 )
 
-
-
 name_of_smoothie = st.text_input("Nazwij swojego drinka")
 st.write("Nazwa drinka to", name_of_smoothie)
 
-
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredienrs_list = st.multiselect('wybierz owocki do koktaju do mask pięciu', my_dataframe, max_selections=5)
